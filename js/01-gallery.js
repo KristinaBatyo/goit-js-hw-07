fc8dee86e6f2813d6f2f724edadaf26644900f56
KristinaBatyo <105168024+KristinaBatyo@users.noreply.github.com>
2022-10-02 20:27:18 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = renderGalleryMarkup(galleryItems);
+function renderGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<div class="gallery__item>
+        a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`;
+        })
+        .join("");
+    }
+    gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+    gallery.addEventListener("click", onGalleryClick);
+    function onGalleryClick(evt) {
+        evt.preventDefault();
+        if (evt.target.nodeName !== "IMG") {
+            return;
+        }
+        const instance = basicLightbox.create (
+            `<img src="${evt.target.dataset.source}" width="800" height="600"/>`
+        );
+        instance.show();
+    }
+
